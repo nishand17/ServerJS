0c24fe711bad0766268f4339e5ccdffcacfb94aa
Nishan D'Souza <nishand@gmail.com>
2016-06-28 17:38:46 +0530

    Added file server with html

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var http = require('http');
+var fs = require('fs');
 
-function onRequest(request, response) {
-  console.log("A user made a request" + request.url);
-  response.writeHead(200, {"Context-Type": "text/plain"});
-  response.write("Here is some data");
+function send404(response) {
+  response.writeHead(404, {"Content-Type": "text/plain"});
+  response.write("Error 404: Page not found!");
   response.end();
+}
+function onRequest(request, response) {
+  if(request.method == 'GET' && request.url == '/') {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream("./index.html").pipe(response);
+  } else {
+    send404(response);
+  }
 }
 
 http.createServer(onRequest).listen(8888);
